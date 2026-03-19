d24e04decb12b7e2a750e13ee4f827391568463a
Luis Oliveira <luispedroliveira96@gmail.com>
2026-03-19 13:14:30 +0000

    refactor(trpc): implement getQueryClient function for improved query client management

--- apps/web/components/trpc/trpc-provider.tsx
+++ apps/web/components/trpc/trpc-provider.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { queryClient, trpc, trpcClient } from "@/lib/trpc/client";
+import { getQueryClient, trpc, trpcClient } from "@/lib/trpc/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
 export default function TrpcProvider({ children }: PropsWithChildren) {
+  const queryClient = getQueryClient();
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
--- apps/web/lib/trpc/client.ts
+++ apps/web/lib/trpc/client.ts
@@ -6,12 +6,32 @@ import {
   httpBatchLink,
 } from '@trpc/react-query';
 
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined;
+
+export function getQueryClient() {
+  if (typeof window === 'undefined') {
+    return makeQueryClient();
+  }
+
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+
+  return browserQueryClient;
+}
+
 export const trpc: CreateTRPCReact<AppRouter, object> = createTRPCReact<
   AppRouter,
   object
 >();
-
-export const queryClient = new QueryClient();
 
 export const trpcClient = trpc.createClient({
   links: [
