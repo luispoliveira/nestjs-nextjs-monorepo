6923a2f71701c292d307847686daf40216ce0cbb
Luis Oliveira <luispedroliveira96@gmail.com>
2026-04-08 18:10:21 +0100

    feat: Remove bodyParser option from NestFactory.create in bootstrap function

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,9 +6,7 @@ import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bodyParser: false,
-  });
+  const app = await NestFactory.create(AppModule, {});
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
 
